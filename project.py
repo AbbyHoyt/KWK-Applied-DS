5237b7e7a41ee04d444a802b6029755b9dee1d3e
AbbyHoyt <abby.hoyt@comcast.net>
2025-08-27 15:34:57 -0700

    Created API request with The Space Devs - Launch Library 2 API.

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
 import requests
 
-response = requests.get("http://randomfox.ca/floof")
+# response = requests.get("http://randomfox.ca/floof")
 # print(response.status_code)
 # print(response.text)
 # print(response.json())
-fox = response.json()
-print(fox['image'])
+# fox = response.json()
+# print(fox['image'])
+
+response = requests.get("https://lldev.thespacedevs.com/2.3.0/astronauts/?format=json")
+print(response.status_code)
