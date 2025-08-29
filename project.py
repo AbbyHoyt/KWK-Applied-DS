0b3af04f5e1cee55797a66931d1b2099904adba2
AbbyHoyt <abby.hoyt@comcast.net>
2025-08-29 14:57:57 -0700

    Cleaned up and deleted old code.

--- project.py
+++ project.py
@@ -1,48 +1,6 @@
 import requests
 import pandas as pd
 
-# response = requests.get("http://randomfox.ca/floof")
-# print(response.status_code)
-# print(response.text)
-# print(response.json())
-# fox = response.json()
-# print(fox['image'])
-
-# response = requests.get("https://lldev.thespacedevs.com/2.3.0/astronauts/?format=json")
-# print(response.status_code)
-# astronauts = response.json()
-# print(astronauts['name'])
-
-# response = requests.get("https://lldev.thespacedevs.com/2.3.0/astronauts/4/?format=json")
-
-# response = requests.get("https://lldev.thespacedevs.com/2.3.0/astronauts/4")
-# print(response.status_code)
-# astronaut = response.json()
-# agency = astronaut['agency']
-# print("Name: " + astronaut['name'] + "\nAgency: " + agency['name'])
-
-# count = 1
-# while count <  10:
-#     response = requests.get("https://lldev.thespacedevs.com/2.3.0/astronauts/" + str(count))
-#     astronaut = response.json()
-#     agency = astronaut['agency']
-#     print("Name: " + astronaut['name'] + "\nAgency: " + agency['name'] + "\nBio: " + astronaut['bio'] + "\n")
-#     count += 1
-
-
-# api_url = "https://lldev.thespacedevs.com/2.3.0/astronauts"
-
-# try:
-#     response = requests.get(api_url)
-#     response.raise_for_status()
-#     data = response.json()
-#     print("Successfully retrived data:")
-#     print(data)
-# except requests.exceptions.RequestException as e:
-#     print(f"Error fetching data: {e}")
-
-# api_url = "https://lldev.thespacedevs.com/2.3.0/astronauts/?limit=100"
-
 api_url = "https://ll.thespacedevs.com/2.3.0/astronauts/?ordering=-time_in_space&is_human=true&limit=100"
 
 response = requests.get(api_url)
