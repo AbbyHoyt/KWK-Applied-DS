911b36ae993e5cc24fc0592f5249b5dfc1629d1b
AbbyHoyt <abby.hoyt@comcast.net>
2025-09-01 19:47:30 -0700

    Started to clean CSV, saving only the results column from the API.

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
+import numpy as np
 
+# Collect
 api_url = "https://ll.thespacedevs.com/2.3.0/astronauts/?ordering=-time_in_space&is_human=true&limit=100"
 
 response = requests.get(api_url)
@@ -10,5 +12,22 @@ if response.status_code == 200:
     df = pd.DataFrame(data)
     df.to_csv('astronaut_data.csv')
     print(df.head())
+
 else:
     print(f"Failed to fetch data: {response.status_code}")
+
+# Clean
+results_array = df['results'].values
+data_array = np.array(results_array) 
+
+np.savetxt('astronauts_data_results.csv', data_array, fmt='%s', delimiter=',')
+
+# Filter
+# # df_filtered = pd.read_csv('astronauts_data_results.csv', sep = ', ')
+# df_filtered = pd.read_csv('astronauts_data_results.csv', sep = ',')
+# df_filtered.to_csv('final_file.csv')
+# print(df_filtered.head())
+
+# df_filtered = pd.read_csv('astronauts_data_results_without_brackets.csv', on_bad_lines='skip')
+# df_filtered = df_filtered.drop(['id', 'url'], axis = 1)
+# df_filtered.to_csv('astronaut_data_filtered.csv')
