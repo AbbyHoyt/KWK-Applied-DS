2a8ddfcf2bff6073bab5048eac224f997389970e
AbbyHoyt <abby.hoyt@comcast.net>
2025-09-02 21:29:30 -0700

    Properly collected, cleaned, and filtered data from API.

--- project.py
+++ project.py
@@ -1,33 +1,23 @@
 import requests
 import pandas as pd
-import numpy as np
 
-# Collect
 api_url = "https://ll.thespacedevs.com/2.3.0/astronauts/?ordering=-time_in_space&is_human=true&limit=100"
-
 response = requests.get(api_url)
+astronaut_data = []
 
-if response.status_code == 200:
-    data = response.json()
-    df = pd.DataFrame(data)
-    df.to_csv('astronaut_data.csv')
-    print(df.head())
+while api_url:
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status() 
+        data = response.json()
+        astronaut_data.extend(data['results'])
+        api_url = data.get('next')
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        break
 
-else:
-    print(f"Failed to fetch data: {response.status_code}")
-
-# Clean
-results_array = df['results'].values
-data_array = np.array(results_array) 
-
-np.savetxt('astronauts_data_results.csv', data_array, fmt='%s', delimiter=',')
-
-# Filter
-# # df_filtered = pd.read_csv('astronauts_data_results.csv', sep = ', ')
-# df_filtered = pd.read_csv('astronauts_data_results.csv', sep = ',')
-# df_filtered.to_csv('final_file.csv')
-# print(df_filtered.head())
-
-# df_filtered = pd.read_csv('astronauts_data_results_without_brackets.csv', on_bad_lines='skip')
-# df_filtered = df_filtered.drop(['id', 'url'], axis = 1)
-# df_filtered.to_csv('astronaut_data_filtered.csv')
+df = pd.json_normalize(astronaut_data, sep = '_')
+# data_to_keep = ['id', 'name', 'status_name', 'type_name', 'agency_name', 'age', 'date_of_birth', 'date_of_death', 'nationality']
+data_to_keep = ['id', 'name', 'status_name', 'type_name', 'agency_name', 'age', 'date_of_birth', 'date_of_death', 'nationality__nationality_name']
+df_final = df.reindex(columns = data_to_keep)
+df_final.to_csv('astronaut_data_cleaned.csv', index = False, encoding = 'utf-8')
